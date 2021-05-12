5d6c9686a93efc1fde8e1619f75558413cf891f5
ngenzir <ngenzir@gmail.com>
2021-05-12 04:10:43 +0300

    edited settings

--- recorder/recorder/settings.py
+++ recorder/recorder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(iyz!f^hsw7n_0be(h-9xzjc!38wtmejo&7dt#0%+z^joh$73!'
+SECRET_KEY = '******'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'audiorec',
 ]
 
 MIDDLEWARE = [
